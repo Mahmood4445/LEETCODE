92f8070926efb56e65f268be834a4b538581729c
Mahmood4445 <93419854+Mahmood4445@users.noreply.github.com>
2024-10-26 22:27:17 +0530

    Create leetcode_242.java

---
+++ leetcode_242.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] schar = s.toCharArray();
+        char[] tchar = t.toCharArray();
+        Arrays.sort(schar);
+        Arrays.sort(tchar);
+
+        return Arrays.equals(schar,tchar);
+    }
+}
