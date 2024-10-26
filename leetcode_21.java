1fcf894dd3ed4e4577732106be5f18ece07e7698
Mahmood4445 <93419854+Mahmood4445@users.noreply.github.com>
2024-10-26 22:19:11 +0530

    Create leetcode_21.java

---
+++ leetcode_21.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       
+
+        ListNode result = new ListNode();
+        ListNode temp = result;
+        
+        while(list1 != null && list2 != null){
+             
+            if(list1.val < list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+
+            }
+            else{
+                 temp.next = list2;
+                 list2 = list2.next;
+
+            }
+            temp =temp.next;
+        }
+       temp.next=(list1!= null) ? list1 : list2;
+      return result.next;  
+    }
+}
